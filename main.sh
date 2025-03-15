9da8cd822bd2de575dbafa91495cdb32d1700994
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-03-15 10:49:21 +0700

    test

--- main.sh
+++ main.sh
@@ -66,7 +66,7 @@ apkmirror() {
     url="https://www.apkmirror.com$(req - "$url" | grep -oP 'class="[^"]*downloadButton[^"]*"[^>]*href="\K[^"]+')"
     url="https://www.apkmirror.com$(req - "$url" | grep -oP 'id="download-link"[^>]*href="\K[^"]+')"
 
-    req "hay-day-v$version.apk" "$url"
+    req "hay-day-v$version.apkm" "$url"
 }
 
 # Tải file APKM của Hay Day
@@ -74,26 +74,15 @@ echo "[*] Đang tải Hay Day từ APKMirror..."
 apkmirror
 
 # Kiểm tra nếu tải không thành công
-APKM_FILE="hay-day-v$version.apk"
+APKM_FILE="hay-day-v$version.apkm"
 if [ ! -f "$APKM_FILE" ]; then
     echo "[!] Lỗi: Không thể tải file APKM!"
     exit 1
 fi
 echo "[✔] Tải thành công: $APKM_FILE"
 
-# Giải nén file APKM
-EXTRACT_DIR="hay-day"
-echo "[*] Giải nén $APKM_FILE..."
-unzip -o "$APKM_FILE" -d "$EXTRACT_DIR" || { echo "[!] Lỗi khi giải nén."; exit 1; }
-
-rm hay-day-v$version.apk
-
-# Kiểm tra cấu trúc thư mục sau khi giải nén
-echo "[*] Kiểm tra cấu trúc thư mục sau khi giải nén..."
-ls -R "$EXTRACT_DIR"
-
 req APKEditor.jar https://github.com/REAndroid/APKEditor/releases/download/V1.4.2/APKEditor-1.4.2.jar
-java -jar APKEditor.jar m -i "$EXTRACT_DIR" >/dev/null
+java -jar APKEditor.jar m -i "$APKM_FILE"
 
 # Xác định apksigner
 if ! command -v apksigner &> /dev/null; then
@@ -112,6 +101,6 @@ echo "[*] Ký lại APK..."
 
 # Ký APK
 "$APKSIGNER" sign --ks public.jks --ks-key-alias public \
-    --ks-pass pass:public --key-pass pass:public --out signed.apk hay-day_merged.apk
+    --ks-pass pass:public --key-pass pass:public --out signed.apk hay-day*.apk
 
 echo "[✔] APK đã được ký lại: $SIGNED_APK"
