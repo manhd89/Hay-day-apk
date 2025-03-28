476803a9e0865926da37ff482603ca99da141ede
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-03-29 06:58:19 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -22,59 +22,22 @@ get_latest_version() {
     grep -Evi 'alpha|beta' | grep -oPi '\b\d+(\.\d+)+(?:\-\w+)?(?:\.\d+)?(?:\.\w+)?\b' | max
 }
 
-# Filtered key words to extract link
-extract_filtered_links() {
-    local dpi="$1" arch="$2" type="$3"
-    awk -v dpi="$dpi" -v arch="$arch" -v type="$type" '
-    BEGIN { block = ""; link = ""; found_dpi = found_arch = found_type = printed = 0 }
-    /<a class="accent_color"/ {
-        if (printed) next
-        if (block != "" && link != "" && found_dpi && found_arch && found_type && !printed) { 
-            print link; printed = 1 
-        }
-        block = $0; found_dpi = found_arch = found_type = 0
-        if (match($0, /href="([^"]+)"/, arr)) link = arr[1]
-    }
-    { if (!printed) block = block "\n" $0 }
-    /table-cell/ && $0 ~ dpi { found_dpi = 1 }
-    /table-cell/ && $0 ~ arch { found_arch = 1 }
-    /apkm-badge/ && $0 ~ (">" type "</span>") { found_type = 1 }
-    END {
-        if (block != "" && link != "" && found_dpi && found_arch && found_type && !printed)
-            print link
-    }
-    '
-}
-
-# Get some versions of application on APKmirror pages 
-get_apkmirror_version() {
-    grep -oP 'class="fontBlack"[^>]*href="[^"]+"\s*>\K[^<]+' | sed 20q | awk '{print $NF}'
-}
-
-# Tải file APKM từ APKMirror theo cách của bạn
-apkmirror() {
-    local name="reddit"
-    local org="redditinc"
-    local dpi=""
-    local arch="universal"
-    local type="BUNDLE"
-
-    url="https://www.apkmirror.com/uploads/?appcategory=$name"
-    version="${version:-$(req - "$url" | get_apkmirror_version | get_latest_version)}"
-    url="https://www.apkmirror.com/apk/$org/$name/$name-${version//./-}-release"
-    url="https://www.apkmirror.com$(req - "$url" | extract_filtered_links "$dpi" "$arch" "$type")"
-    url="https://www.apkmirror.com$(req - "$url" | grep -oP 'class="[^"]*downloadButton[^"]*"[^>]*href="\K[^"]+')"
-    url="https://www.apkmirror.com$(req - "$url" | grep -oP 'id="download-link"[^>]*href="\K[^"]+')"
-
-    req "hay-day-v$version.apkm" "$url"
+apkpure() {
+    name="spotify-music-and-podcasts-for-android"
+    package="com.spotify.music"
+    url="https://apkpure.net/$name/$package/versions"
+    version="${version:-$(req - $url | grep -oP 'data-dt-version="\K[^"]*' | sed 10q | get_latest_version)}"
+    url="https://apkpure.net/$name/$package/download/$version"
+    url=$(req - $url | grep -oP '<a[^>]*id="download_link"[^>]*href="\K[^"]*' | head -n 1)
+    req $name-v$version.apk "$url"
 }
 
 # Tải file APKM của Hay Day
 echo "[*] Đang tải Hay Day từ APKMirror..."
-apkmirror
+apkpure
 
 # Kiểm tra nếu tải không thành công
-APKM_FILE="hay-day-v$version.apkm"
+APKM_FILE="spotify-v$version.apkm"
 if [ ! -f "$APKM_FILE" ]; then
     echo "[!] Lỗi: Không thể tải file APKM!"
     exit 1
